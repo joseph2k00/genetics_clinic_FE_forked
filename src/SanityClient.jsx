c6678cc3e33bb0248a6ff68053d4d28dc7749a59
joseph2k00 <105558046+joseph2k00@users.noreply.github.com>
2025-12-24 03:31:44 +0000

    Sanity Client Interface

---
+++ src/SanityClient.jsx
@@ -0,0 +1,14 @@
+import { createClient } from '@sanity/client'
+
+export const sanityClient = createClient({
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
+  apiVersion: '2024-01-01',
+  useCdn: true,
+})
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source) {
+  return builder.image(source);
+}
