edcddb909cdd5bff9027e45d29f1f0c184581e31
joseph2k00 <105558046+joseph2k00@users.noreply.github.com>
2025-12-25 15:51:27 +0000

    Education and Work Experience Page

--- src/pages/Education/Education.jsx
+++ src/pages/Education/Education.jsx
@@ -1,11 +1,42 @@
-import WorkInProgress from "../WorkInProgress/WorkInProgress";
+import { useEffect, useState } from "react";
+import { sanityClient } from "../../SanityClient";
+import "./Education.css"
 
 const Education = () => {
+    const [experiences, setExperiences] = useState([]);
+
+    useEffect(() => {
+        sanityClient
+            .fetch(`
+                *[_type == "exps" && _id == "exps"][0]{
+                    title,
+                    exps[]{
+                        _key,
+                        date,
+                        title
+                    }
+                }
+            `)
+            .then((data) => {
+                setExperiences(data?.exps || []);
+            })
+            .catch(console.error);
+    }, []);
+
     return (
-        <>
-            <WorkInProgress/>
-        </>
-    )
-}
+        <div className="section">
+            <div className="section-title">Education & Work Experience</div>
+
+            <div className="experience-container">
+                {experiences.map((item) => (
+                    <div className="experience-card" key={item._key}>
+                        <div className="experience-date">{item.date}</div>
+                        <div className="experience-title">{item.title}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default Education;
