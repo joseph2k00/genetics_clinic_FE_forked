c5a8dfdb247867390f067c78463e513a988a449c
joseph2k00 <105558046+joseph2k00@users.noreply.github.com>
2025-12-25 15:42:02 +0000

    Certificates and Memberships page

--- src/pages/Certificates/Certificates.jsx
+++ src/pages/Certificates/Certificates.jsx
@@ -1,11 +1,45 @@
-import WorkInProgress from "../WorkInProgress/WorkInProgress";
+import { useEffect, useState } from "react";
+import { sanityClient } from "../../SanityClient";
+import "./Certificates.css";
+import Loader from "../../components/Loader/Loader";
 
 const Certificates = () => {
+    const [certificates, setCertificates] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        sanityClient
+            .fetch(`
+                *[_type == "certificates" && _id == "certificates"][0]{
+                    title,
+                    awards[]{
+                        _key,
+                        title
+                    }
+                }
+            `)
+            .then((data) => {
+                setCertificates(data?.awards || []);
+                setLoading(false);
+            })
+            .catch(console.error);
+    }, []);
+
+    if (loading) return <Loader/>;
+
     return (
-        <>
-            <WorkInProgress/>
-        </>
-    )
-}
+        <div className="section">
+            <div className="section-title">Certificates & Memberships</div>
+
+            <div className="certificates-single-column">
+                {certificates.map((item) => (
+                    <div className="certificate-pill" key={item._key}>
+                        {item.title}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default Certificates;
