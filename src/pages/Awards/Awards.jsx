e44efa4f1289628e8e1460e6134e2620692df4d1
joseph2k00 <105558046+joseph2k00@users.noreply.github.com>
2025-12-25 16:01:09 +0000

    Awards Page

--- src/pages/Awards/Awards.jsx
+++ src/pages/Awards/Awards.jsx
@@ -1,11 +1,46 @@
-import WorkInProgress from "../WorkInProgress/WorkInProgress";
+import { useEffect, useState } from "react";
+import { sanityClient } from "../../SanityClient";
+import "./Awards.css";
+import Loader from "../../components/Loader/Loader";
 
 const Awards = () => {
+    const [awards, setAwards] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        sanityClient
+            .fetch(`
+                *[_type == "awards" && _id == "awards"][0]{
+                    title,
+                    awards[]{
+                        _key,
+                        date,
+                        title
+                    }
+                }
+            `)
+            .then((data) => {
+                setAwards(data?.awards || []);
+                setLoading(false);
+            })
+            .catch(console.error);
+    }, []);
+
+    if (loading) return <Loader/>;
     return (
-        <>
-            <WorkInProgress/>
-        </>
-    )
-}
+        <div className="section">
+            <div className="section-title">Awards</div>
+
+            <div className="awards-container">
+                {awards.map((item) => (
+                    <div className="award-card" key={item._key}>
+                        <div className="award-date">{item.date}</div>
+                        <div className="award-title">{item.title}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default Awards;
